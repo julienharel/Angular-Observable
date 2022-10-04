39f13ef3d5fab3a895ead71f44847cc11d95ea9c
julienharel <97918065+julienharel@users.noreply.github.com>
2022-10-04 14:10:53 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,19 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
 
-  // Création d'un observable 
-  observable = new Observable
+  // Création d'un observable
+  // sub = subscriber
+  observable = new Observable((sub) => {
+    sub.next(1);
+  });
+
+  constructor() {
+    // souscrire à l'observable
+    this.observable.subscribe((data) => console.log(data));
+  }
 }
